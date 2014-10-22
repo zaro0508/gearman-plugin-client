1df895a9ca42909e38d2f992572e77c2b530d91d
Khai Do <zaro0508@gmail.com>
2014-10-22 11:23:50 -0700

    update client to allow better debugging of gearman plugin
    * add additional documentation
    * print job result from a job run.

--- gear_client.py
+++ gear_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import gear
 import simplejson
 import uuid
@@ -7,8 +8,11 @@ import sys
 
 # simple python client
 # usage:
-#  To run a build:
-#      python gear_client.py -s MyGearmanSever --function=build:myProject \
+#  To run a build (interactive):
+#      python gear_client.py -s MyGearmanSever --function=build:myProject --wait
+#
+#  To run multiple builds (asyncronously and with parameters):
+#      python gear_client.py -s MyGearmanSever --function=build:myProject --jobs=2 \
 #            --params='{"OFFLINE_NODE_WHEN_COMPLETE":"false","param1":"moon","param1":"sun"}'
 #
 #  To stop/abort a build:
@@ -18,12 +22,16 @@ import sys
 #  To change the build description:
 #      python gear_client.py -s MyGearmanSever --function=set_description:MyGearmanSever \
 #            --params='{"name":"myProject","number":"105","html_description":"<h1>My New Description</h1>"}'
-    
+#
+#  To run a build and immediately and then set the slave offline:
+#      python gear_client.py -s MyGearmanSever --function=build:myProject \
+#            --params='{"OFFLINE_NODE_WHEN_COMPLETE":"true"}'
+
 class Client(object):
     def __init__(self):
         self.args = None
         self.config = None
-        
+
     def parse_arguments(self):
         parser = argparse.ArgumentParser(description='Gearman Client')
         parser.add_argument('-s', dest='server',
@@ -37,7 +45,7 @@ class Client(object):
                             help='Gearman function')
         parser.add_argument('--jobs', dest='jobs', default=1,
                             help='Number of jobs (default=1)')
-        parser.add_argument('--params', dest='params', 
+        parser.add_argument('--params', dest='params',
                             default={"OFFLINE_NODE_WHEN_COMPLETE":"false"},
                             help='Parameters to pass to build')
         parser.add_argument('--log-config', dest='log_config',
@@ -49,32 +57,34 @@ class Client(object):
         gclient.addServer(self.args.server)
         gclient.waitForServer()  # Wait for at least one server to be connected
 
-        print time.asctime( time.localtime(time.time()))
         if not isinstance(self.args.params, dict):
             build_params = simplejson.loads(self.args.params)
         else:
             build_params = self.args.params
-            
+
         for x in range(0, int(self.args.jobs)):
             job_id = uuid.uuid4().hex
             build_params.update({'uuid':job_id})
-            gjob = gear.Job(self.args.function, 
-                           simplejson.dumps(build_params), 
+            gjob = gear.Job(self.args.function,
+                           simplejson.dumps(build_params),
                            unique=job_id)
-            print "Sending job "+self.args.function+" to "+self.args.server+" with params="+ str(build_params)
+            if self.args.wait:
+                print "\n" + time.asctime( time.localtime(time.time()))
+            print "Sending job: " + self.args.function + " to " + self.args.server + " with params=" + str(build_params)
             gclient.submitJob(gjob)
-        
-        # wait for last job to complete before exiting
-        if self.args.wait:
-            finished = False
-            while True:
-                if (gjob.complete):
-                    finished = True
-                time.sleep(1);
-                if finished:
-                    break
 
-        print time.asctime( time.localtime(time.time()) )
+            # wait for last job to complete before exiting
+            if self.args.wait:
+                finished = False
+                while True:
+                    if (gjob.complete):
+                        print time.asctime( time.localtime(time.time()) )
+                        print "Job Result: " + str(gjob.data) + "\n"
+                        finished = True
+                    time.sleep(1);
+                    if finished:
+                        break
+
 
 def main():
     client = Client()
@@ -84,4 +94,3 @@ def main():
 if __name__ == '__main__':
     sys.path.insert(0, '.')
     main()
-            
